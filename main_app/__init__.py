09c6adc38d56635bd739e657c8385476c8d777ad
Andreas Calleja <38852990+andreasmalta1@users.noreply.github.com>
2023-07-07 17:15:23 +0200

    Added blueprints

--- main_app/__init__.py
+++ main_app/__init__.py
@@ -5,3 +5,11 @@ from os import getenv
 load_dotenv()
 
 app = Flask(__name__)
+
+from main_app.url import url
+# from main_app.random_color import random_color
+# from main_app.random_text import random_text
+
+app.register_blueprint(url, url_prefix="/shorten")
+# app.register_blueprint(random_color, url_prefix="/random-color")
+# app.register_blueprint(random_text, url_prefix="/random-picker")
