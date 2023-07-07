55aa97e887106179bc836837ff9992f481e5f3b9
Andreas Calleja <38852990+andreasmalta1@users.noreply.github.com>
2023-07-07 17:15:43 +0200

    Working on url shortener

---
+++ main_app/url.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, render_template, request, flash
+from pyshorteners import Shortener
+
+url = Blueprint("url", __name__)
+
+
+@url.route("/", methods=["GET", "POST"])
+def shorten_url():
+    if request.method == 'POST':
+        long_url = request.form['url']
+
+        if not long_url:
+            flash('Title is required!')
+
+        type_tiny = Shortener()
+
+        short_url = type_tiny.tinyurl.short(long_url)
+        print(short_url)
+
+    if request.method == 'GET':
+        return render_template("url.html")
