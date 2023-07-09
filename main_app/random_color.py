a67ebbf1af97d3f053e849630f992f23e13803f5
Andreas Calleja <andreascalleja@gmail.com>
2023-07-09 20:29:12 +0200

    Added random color picker

---
+++ main_app/random_color.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, render_template, request, flash
+from pyshorteners import Shortener
+import random
+
+random_color = Blueprint("random_color", __name__)
+
+
+@random_color.route("/", methods=["GET", "POST"])
+def get_random_color():
+    if request.method == "POST":
+        r = lambda: random.randint(0, 255)
+        color = "#%02X%02X%02X" % (r(), r(), r())
+        return render_template("color.html", color=color)
+
+    if request.method == "GET":
+        return render_template("color.html")
--- main_app/url.py
+++ main_app/url.py
@@ -6,16 +6,16 @@ url = Blueprint("url", __name__)
 
 @url.route("/", methods=["GET", "POST"])
 def shorten_url():
-    if request.method == 'POST':
-        long_url = request.form['url']
+    if request.method == "POST":
+        long_url = request.form["url"]
 
         if not long_url:
-            flash('Title is required!')
+            flash("Title is required!")
 
         type_tiny = Shortener()
 
         short_url = type_tiny.tinyurl.short(long_url)
-        print(short_url)
+        return render_template("url.html", short_url=short_url)
 
-    if request.method == 'GET':
+    if request.method == "GET":
         return render_template("url.html")
